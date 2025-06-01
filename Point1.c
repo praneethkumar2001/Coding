2726e0cb19277f3cdce6fae41f365c943aad80d8
praneethkumar2001 <praneethkumar200105@gmail.com>
2025-06-01 18:00:23 +0530

    Update Point1.c

--- Point1.c
+++ Point1.c
@@ -16,3 +16,47 @@ Output:
 Address 0x7ffee214ac64
 Value =10
 size of Integer : 4
+
+
+#include<stdio.h>
+int main()
+{
+    int *A;
+    int B=10;
+    A = &B;
+    printf("\nAddress : %p",A);
+    printf("\nValue = %d", *A);
+    printf("\nsize of Integer : %d", sizeof(int));
+    printf("\nAddress : %p",--A);
+    return 0;
+}
+
+Output:
+
+Address : 0x7ffcce17ad24
+Value = 10
+size of Integer : 4
+Address : 0x7ffcce17ad20
+
+
+#include<stdio.h>
+int main()
+{
+    int *A;
+    int B=10;
+    A = &B;
+    printf("\nAddress : %p",A);
+    printf("\nValue = %d", *A);
+    printf("\nsize of Integer : %d", sizeof(int));
+    printf("\nAddress : %p",++A);
+    return 0;
+}
+
+Output:
+
+Address : 0x7ffdba9add54
+Value = 10
+size of Integer : 4
+Address : 0x7ffdba9add58
+
+
